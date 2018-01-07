c20882534d8baabae7280cefc05e3cb19093da92
lwohtsu <ohtsu-y1@libroworks.co.jp>
2018-01-07 13:49:16 +0900

    Initial commit

---
+++ spec/markdown-book-preview-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MarkdownBookPreviewView from '../lib/markdown-book-preview-view';
+
+describe('MarkdownBookPreviewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
